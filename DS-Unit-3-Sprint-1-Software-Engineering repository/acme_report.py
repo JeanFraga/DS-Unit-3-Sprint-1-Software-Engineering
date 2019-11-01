b63894f826edee29755308dc94b4e9ef3eed31ca
JeanFraga <JeanFraga@users.noreply.github.com>
2019-11-01 15:16:48 -0400

    added a test I had unfinished and a print error in acme report

--- DS-Unit-3-Sprint-1-Software-Engineering repository/acme_report.py
+++ DS-Unit-3-Sprint-1-Software-Engineering repository/acme_report.py
@@ -8,6 +8,7 @@ from acme import Product
 ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 
+
 def generate_products(num_products=30):
     """
     generate a given number of products (default 30), randomly, and return them
@@ -15,44 +16,52 @@ def generate_products(num_products=30):
     """
     products = []
     for product in range(num_products):
-        adjective = sample(ADJECTIVES,1)[0]
-        noun = sample(NOUNS,1)[0]
+        adjective = sample(ADJECTIVES, 1)[0]
+        noun = sample(NOUNS, 1)[0]
         space = " "
         product = adjective + space + noun
         products.append(product)
 
     prices = []
     for price in range(num_products):
-        price = randint(5,100)
+        price = randint(5, 100)
         prices.append(price)
 
     weights = []
     for weight in range(num_products):
-        weight = randint(5,100)
+        weight = randint(5, 100)
         weights.append(weight)
 
     flammabilities = []
     for flammability in range(num_products):
-        flammability = uniform(0.0,2.5)
+        flammability = uniform(0.0, 2.5)
         flammabilities.append(flammability)
 
     return products, prices, weights, flammabilities
 
-def inventory_report(products):
+
+def inventory_report():
     """
     takes a list of products, and prints a "nice" summary
     """
     products, prices, weights, flammability = generate_products()
 
-    for item in range(len(products)):
-        print("{} has a price of {}, a weight of {}, and a flammability of {}"
-                 .format(products[item],prices[item],flammability[item]))
+    print("ACME CORPORATION OFFICIAL INVENTORY REPORT")
+    print("Unique product names: " + str(len(products)))
 
     def mean_function(category):
+        """
+        a simple mean function to calculate for each category.
+        """
         for i in category:
-            sum_i = 0 + category[i] 
+            sum_i = 0 + i
+        category_mean = sum_i/len(category)
+        return category_mean
 
-    mean_prices = 
-    mean_weights = 
-    mean_flammability = 
-    return 
+    mean_prices = mean_function(prices)
+    mean_weights = mean_function(weights)
+    mean_flammability = mean_function(flammability)
+
+    print("Average price: " + str(mean_prices))
+    print("Average weight: " + str(mean_weights))
+    print("Average flammability: " + str(mean_flammability))
--- DS-Unit-3-Sprint-1-Software-Engineering repository/acme_test.py
+++ DS-Unit-3-Sprint-1-Software-Engineering repository/acme_test.py
@@ -4,7 +4,8 @@ Testing for "acme.py" and "acme_report.py" using unittest
 
 import unittest
 from acme import Product
-#from acme_report import generate_products, ADJECTIVES, NOUNS
+from acme_report import generate_products, ADJECTIVES, NOUNS
+
 
 class AcmeProductTests(unittest.TestCase):
     """Making sure Acme products are the tops!"""
@@ -13,6 +14,29 @@ class AcmeProductTests(unittest.TestCase):
         prod = Product('Test Product')
         self.assertEqual(prod.price, 10)
 
+    def test_default_product_price(self):
+        """Test default product price being 10."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.weight, 10)
+
+    def test_default_product_price(self):
+        """Test default product price being 10."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.name, "Test Product")
+
+class AcmeProductTests(unittest.TestCase):
+    """Making sure acme_report is superb!"""
+    def test_default_num_products(self):
+        """Test default product number being 30"""
+        gen_prod = generate_products()
+        self.assertEqual(len(gen_prod[0]), 30)
+
+    def test_legal_names(self):
+        """Test possible combinations"""
+        ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
+        NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
+        gen_prod = generate_products()[0]
+        self.assertIn(generate_products()[0], [str(ADJECTIVES) + ' ' + str(NOUNS)])
 
 if __name__ == '__main__':
     unittest.main()
